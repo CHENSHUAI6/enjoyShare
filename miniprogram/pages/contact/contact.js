1df5b9d51b70e33328b121584662494176bbde08
wangyue <wangyue03222@klicen.com>
2020-05-13 17:41:29 +0800

    contact：下拉刷新，下滑加载

--- miniprogram/pages/contact/contact.js
+++ miniprogram/pages/contact/contact.js
@@ -5,6 +5,53 @@ Page({
    * 页面的初始数据
    */
   data: {
+    contactList:[
+      {
+        name: "Hilary Chen",
+        position: "ABAP顾问",
+        qq: "178057198",
+        email: "178057198@qq.com",
+        wechatName: "easyStyle6",
+      },
+      {
+        name: "Hilary Chen1",
+        position: "ABAP顾问",
+        qq: "178057198",
+        email: "178057198@qq.com",
+        wechatName: "easyStyle6",
+      },{
+        name: "Hilary Chen2",
+        position: "ABAP顾问",
+        qq: "178057198",
+        email: "178057198@qq.com",
+        wechatName: "easyStyle6",
+      },{
+        name: "Hilary Chen3",
+        position: "ABAP顾问",
+        qq: "178057198",
+        email: "178057198@qq.com",
+        wechatName: "easyStyle6",
+      },{
+        name: "Hilary Chen4",
+        position: "ABAP顾问",
+        qq: "178057198",
+        email: "178057198@qq.com",
+        wechatName: "easyStyle6",
+      },{
+        name: "Hilary Chen5",
+        position: "ABAP顾问",
+        qq: "178057198",
+        email: "178057198@qq.com",
+        wechatName: "easyStyle6",
+      },
+      {
+        name: "Demi",
+        position: "MM 顾问",
+        qq: "1404318263",
+        email: "1404318263@qq.com",
+        wechatName: "dizhangmen",
+      },
+    ],
 
   },
 
@@ -47,6 +94,8 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    console.log("这里更新数据");
+    // 更新
 
   },
 
@@ -54,7 +103,19 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log("底部", this.data);
+    const temp = this.data.contactList;
+    temp.push({
+      name: "yueyue",
+      position: "boss",
+      qq: "88888888",
+      email: "88888@yuer.com",
+      wechatName: "yueyue",
+    });
+    this.setData({
+      contactList: temp,
+    });
+    
   },
 
   /**
